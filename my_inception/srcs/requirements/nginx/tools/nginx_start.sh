8daf833cf7b651ddb0feedae87bf13aaaa8d418f
mucahitcakmak <mucoox18@gmail.com>
2024-10-30 19:36:25 +0300

    Proje dosyalarım tamamlandı...

--- my_inception/srcs/requirements/nginx/tools/nginx_start.sh
+++ my_inception/srcs/requirements/nginx/tools/nginx_start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=ISTANBUL/L=MASLAK/O=42Istanbul/CN=mucakmak.42.fr";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=wordpress/CN=mucakmak.42.fr";
 echo "Nginx: ssl is set up!";
 fi
 
